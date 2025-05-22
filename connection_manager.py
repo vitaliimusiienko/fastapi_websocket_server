fb18f109305edffa0afcd17c504d070845b4ee3d
Vitalii Musiienko <vitaliimusiienko10@gmail.com>
2025-05-22 22:10:05 +0300

    creating a comletion logic< adding two possible completion options either when there are no active users oor 30 minutes after start the server

--- connection_manager.py
+++ connection_manager.py
@@ -1,23 +1,40 @@
+import logging
 from fastapi import WebSocket
+from typing import List
+
+logger = logging.getLogger(__name__)
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections: list[WebSocket] = []
+        self.active_connections: List[WebSocket] = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
         self.active_connections.append(websocket)
+        logger.info(f"Client connected: Total: {self.count_connections()}")
 
     def disconnect(self, websocket: WebSocket):
         if websocket in self.active_connections:
             self.active_connections.remove(websocket)
+            logger.info(f"Client disconnected: Total: {self.count_connections()}")
+
+    async def close_all_connections(self):
+        for connection in self.active_connections:
+            try:
+                await connection.close()
+                logger.info("Closed connection")
+            except Exception as e:
+                logger.error(f"Error closing connection: {e}")
+        self.active_connections.clear()
+        logger.info("All connections closed")
             
     async def send_message(self, message: str):
         for connection in self.active_connections:
             try:
                 await connection.send_text(message)
+                logger.info(f"Message sent to client: {message}")
             except Exception as e:
-                print(f"Error sending message: {e}")
+                logger.error(f"Error sending message to client: {e}")
                 self.disconnect(connection)
 
     def count_connections(self):
--- main.py
+++ main.py
@@ -1,36 +1,80 @@
 import asyncio 
+import signal
+import logging
+import os
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse, JSONResponse
 from connection_manager import ConnectionManager
+from shutdown_handler import ShutdownHandler
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 manager = ConnectionManager()
-
+minutes_to_shutdown = 1
+shutdown_handler = ShutdownHandler(manager, timeout_minutes=minutes_to_shutdown)
 message = "Test"
+background_tasks = []
+
+def signal_shutdown_handler(signum, frame):
+            logger.info(f"Received shutdown signal: {signum}")
+            shutdown_handler.handle_signal(signum, frame)
+
 
 @app.on_event("startup")
 async def startup_event():
-    asyncio.create_task(send_test_notification())
+    signal.signal(signal.SIGINT, signal_shutdown_handler)
+    signal.signal(signal.SIGTERM, signal_shutdown_handler)
+
+    background_tasks.append(asyncio.create_task(shutdown_handler.wait_for_shutdown(on_shutdown)))
+    background_tasks.append(asyncio.create_task(send_test_notification()))
+    
+    logger.info("Server started and waiting for shutdown signal...")
+
+    
+
 
 async def send_test_notification():
-    while True:
-        await asyncio.sleep(10)
-        await manager.send_message(message)
+    while not shutdown_handler.shutdown_event.is_set():
+        await asyncio.sleep(5)
+        if manager.count_connections() > 0:
+            await manager.send_message(message)
+            logger.info(f"Sent message to {manager.count_connections()} clients.")
+
+        else:
+            logger.info("No active connections to send message to.Shutting down in 1 minute.")
+            shutdown_handler.request_signal()
+            break
+
+async def on_shutdown():
+    logger.info("Shutting down server...")
+    for task in background_tasks:
+        task.cancel()
+        try:
+            await task
+        except asyncio.CancelledError:
+            logger.info("Task cancelled")
+    os._exit(0)
+
+
+        
+    
+
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await manager.connect(websocket)
+    logger.info("Client connected")
     try:
         while True:
             data = await websocket.receive_text()
-            print(f"Message received: {data}")
+            logger.info(f"Received message from client: {data}")
             await manager.send_message(data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        print("Client disconnected")
+        logger.info("Client disconnected")
     except Exception as e:
-        print(f"Error: {e}")
+        logger.error(f"Error: {e}")
         manager.disconnect(websocket)
         await websocket.close()
-        
